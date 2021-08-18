a43f9133268a3efc1411f23045c7af743d7cac29
Vladislav <vladislav.igorevich.workmail@gmail.com>
2021-08-18 16:31:48 +0300

    Added main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private void Awake()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+}
