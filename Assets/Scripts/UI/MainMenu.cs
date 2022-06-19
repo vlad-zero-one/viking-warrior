b6be02b767bb8871bde1866a486a8ebf74caca6d
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2022-06-19 20:33:07 +0300

    Async loading TestScene

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,12 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     private const string TestSceneName = "TestScene";
 
+    [SerializeField] private GameObject _title;
+    [SerializeField] private GameObject _buttons;
+    [SerializeField] private Slider _loadingProgressBar;
+
+    private AsyncOperation loadingOperation;
+
     public void LoadTestScene()
     {
-        SceneManager.LoadScene(TestSceneName);
+        loadingOperation = SceneManager.LoadSceneAsync(TestSceneName);
+
+        _title.SetActive(false);
+        _buttons.SetActive(false);
+        _loadingProgressBar.gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (loadingOperation != null)
+        {
+            _loadingProgressBar.value = (loadingOperation.progress / 0.9f);
+        }
     }
 }
